0af3bcf2825f141ebc675085b37962465356c2c5
ta1itha <talithadev93@gmail.com>
2024-04-11 14:23:21 +0200

    add if else and comparison exercises

---
+++ index.js
@@ -0,0 +1,62 @@
+// 1.If else statement – comparison operators
+
+// Note - Start by defining the variables you need. Write the code underneath each exercise
+
+/*
+    1.1) Write a if else statement to compare two numbers (any numbers) and console log
+         which one is larger, or if they are equal. Use the > and === operator
+
+*/
+
+// insert code below:
+
+/*
+    1.2) Modify your statement from Exercise 1.1 to compare three numbers and console log the
+         largest one. Use the >, || (OR), and && (AND) operators to combine conditions.
+*/
+
+// insert code below:
+
+/*
+
+    1.3) Write a JavaScript program that checks if a number is within a given range.
+         The range is 10 to 50 inclusive. Use the >= and <= operators.
+         Console.log a message indicating whether the number is within the range.
+
+*/
+
+// insert code below:
+
+/*
+    1.4) JavaScript has two types of equality operators: == (loose equality) and
+         === (strict equality). Write an if else statement that compares two values with both operators
+         and log out a message to console explaining if they are "loosely" or
+         "strictly" equal, or not equal at all. Use different data types for the comparison to
+         see how type coercion works with ==.
+*/
+
+/*
+    1.5) Write an if else statement that checks if two values are not equal, using both the != (loose not-equal)
+         and !== (strict not-equal) operators. First, compare two numbers that are not equal. Then, compare a number
+         and a string that wouldn't be equal strictly but would be considered equal with type coercion. Explain the
+         outcome with comments or print statements.
+
+*/
+
+/*
+    1.6) Create a more complex program that involves multiple variables and logical conditions. Suppose you have three
+         variables: age, driverLicense, and visionGood. Write a program that checks if a person is eligible to drive.
+         The conditions are:
+
+        They must be 18 years or older.
+        They must have a driver's license (driverLicense should be true).
+        Their vision must be good (visionGood should be true).
+
+
+*/
+
+// Loops – learning the syntax
+
+// Functions
+
+// Array methods
